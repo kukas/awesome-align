972bc51d790ad6beffa57161591a419a03dc3a60
Jirka Balhar <balhar.j@gmail.com>
2023-12-11 12:58:14 +0100

    mapping tokenizations WIP

---
+++ map_tokenizations.py
@@ -0,0 +1,30 @@
+def granularize_tokenization(coarse_tokenization, tokenizer):
+    """
+    Maps coarse tokenization into a more granular tokenization.
+    """
+    granular_tokenization = []
+    granular_to_coarse_mapping = {}
+    j = 0
+    for i, token in enumerate(coarse_tokenization):
+        subtokens = tokenizer(token)
+        for subtoken in subtokens:
+            granular_tokenization.append(subtoken)
+            granular_to_coarse_mapping[j] = i
+            j += 1
+    return granular_tokenization, granular_to_coarse_mapping
+
+def map_tokenization(granular_alignment, src_token_mapping, trg_token_mapping):
+    """
+    Maps the granular_alignment into a more corse alignment
+    using the src_token_mapping and trg_token_mapping.
+    """
+    # We do not want duplicate alignments so we keep track of them in a set
+    alignment_set = set()
+    alignment = []
+    for i, j in granular_alignment:
+        mapped_alignment = (src_token_mapping[i], trg_token_mapping[j])
+        if mapped_alignment in alignment_set:
+            continue
+        alignment.append(list(mapped_alignment))
+        alignment_set.add(mapped_alignment)
+    return alignment
