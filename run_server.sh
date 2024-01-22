8ba40ff3b8eb0f731b125520442864cd57a4856b
Jirka <balhar.j@gmail.com>
2024-01-22 15:20:13 +0000

    rename the virtual env

--- run_server.sh
+++ run_server.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # activate the virtual environment
-source /home/balhar/miniconda3/bin/activate aligner_csuk
+source /home/balhar/miniconda3/bin/activate aligner
 
 # run the server
 gunicorn --log-syslog -b :19080 -w 1 "align_server:create_aligner()"
