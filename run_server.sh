16401d07f7a0927b9c2ea03f5eeeda01954a04e9
Jirka <balhar.j@gmail.com>
2024-01-23 11:55:16 +0000

    remove unneeded log-syslog and listen only to localhost

--- run_server.sh
+++ run_server.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 # activate the virtual environment
 source /home/balhar/miniconda3/bin/activate aligner
 
-# run the server
-gunicorn --log-syslog -b :19080 -w 1 "align_server:create_aligner()"
+# run the server (listen on localhost, the service will be reverse proxied using Nginx)
+gunicorn -b 127.0.0.1:19080 -w 1 "align_server:create_aligner()"
